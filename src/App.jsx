0eb8d9ec16941982056dfe8d385ee19798bb5e26
Ozodbek <ozodbektursunaliyev11@gmail.com>
2025-07-14 10:48:07 +0500

    Add updated video files for features and hero sections

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h2 className="text-4xl font-bold  text-violet-300">
+        Welcome to Awwards
+      </h2>
+    </main>
+  );
+}
+
+export default App;
