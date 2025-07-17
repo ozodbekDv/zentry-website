c485ca02fabd9f8e8fc15de41f4275a76580bc96
Ozodbek <ozodbektursunaliyev11@gmail.com>
2025-07-17 19:59:54 +0500

    Add Button component and integrate react-icons in Hero component; update package dependencies

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Button({ title, id, containerClass, leftIcon, rightIcon }) {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-voilet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="overflow-hidden inline-flex relative font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+    </button>
+  );
+}
+
+export default Button;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
 import { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 function Hero() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const nextVideoRef = useRef(null);
 
   const handleVideoLoad = () => {
@@ -48,27 +50,47 @@ function Hero() {
           </div>
           <video
             ref={nextVideoRef}
-            className="absolute-center invisible absolute z-20 size-63 object-cover object-center"
             src={getVideoSrc(currentIndex)}
-            muted
             loop
+            muted
             id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
           <video
             src={getVideoSrc(
-              currentIndex === totalVideos + 1 ? 1 : currentIndex
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
             )}
             autoPlay
-            muted
             loop
+            muted
             className="absolute left-0 top-0 size-full object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
         </div>
-        <h1 className="special-font hero-heading font-zentry uppercase absolute bottom-5 right-5 text-blue-75 z-40">
-          G <b>a</b> ming
+        <h1 className="special-font hero-heading font-zentry absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>A</b>MING
         </h1>
+
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              Redifi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robertRegular text-blue-100">
+              Enter the Metagame Layer
+              <br />
+              Unleash the Play Economy
+            </p>
+
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
