785ece16f032b13f28c86a261d18dc3b5a43630b
Ozodbek <ozodbektursunaliyev11@gmail.com>
2025-07-26 12:39:58 +0500

    Replace AnimatedText with AnimatedTitle component and implement GSAP animations for title display

--- src/components/AnimatedText.jsx
+++
@@ -1,9 +0,0 @@
-function AnimatedText() {
-  return (
-    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-      Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-    </div>
-  );
-}
-
-export default AnimatedText;
