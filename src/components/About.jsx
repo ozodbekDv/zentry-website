4777f1c210865e35b5a8154e6c6b7d8353d7d1e9
Ozodbek <ozodbektursunaliyev11@gmail.com>
2025-07-31 12:25:29 +0500

    Refactor About component: update text elements and enhance layout for improved readability

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,6 +18,7 @@ function About() {
         pinSpacing: true,
       },
     });
+
     clipAnimation.to(".mask-clip-path", {
       width: "100vw",
       height: "100vh",
@@ -27,28 +28,31 @@ function About() {
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
-        </h2>
+        </p>
 
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure"
-          containerClass="mt-5 text-black text-center"
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
         />
 
         <div className="about-subtext">
-          <p>The Game of Games begins-your life, now an epic MMORPG</p>
-          <p>Zentry unites every player from countless games and platforms</p>
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
+      </div>
 
-        <div className="h-dvh w-screen" id="clip">
-          <div className="mask-clip-path about-image">
-            <img
-              src="img/about.webp"
-              className="absolute left-0 top-0 size-full object-cover border-2 rounded-[30px]"
-              alt=""
-            />
-          </div>
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
         </div>
       </div>
     </div>
