49f8686ebe15608f8f7a9c0c7e779fbe6434a4e1
Ozodbek <ozodbektursunaliyev11@gmail.com>
2025-08-02 21:42:05 +0500

    Add Features component and integrate it into App layout

---
+++ src/components/Features.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Features() {
+  return (
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="font-curcularWeb text-lg text-blue-50">
+            Into the Metagame Layer
+          </p>
+        </div>
+        <p className="max-w-md font-curcularWeb text-lg text-blue-50 opacity-50">
+          Immerse yourself in a rich and ever-expanding universe where a vibrant
+          array of products converge into an interconnected overlay experience
+          on your world.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Features;
