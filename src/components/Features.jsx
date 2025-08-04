ac24154100246dbf3c6cac652adbfa2bbc1d7f0a
Ozodbek <ozodbektursunaliyev11@gmail.com>
2025-08-04 08:38:46 +0500

    Refactor Features component: update import statement and add Bentocard for enhanced feature display

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,24 @@
-import React from "react";
+const Bentocard = ({ src, describtion, isComingSoon, title }) => {
+  return (
+    <div>
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {describtion && (
+            <p className="mt-3 max-w-64 text-xsm ms:text-base">{describtion}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function Features() {
   return (
@@ -8,12 +28,37 @@ function Features() {
           <p className="font-curcularWeb text-lg text-blue-50">
             Into the Metagame Layer
           </p>
+          <p className="max-w-md font-curcularWeb text-lg text-blue-50 opacity-50">
+            Immerse yourself in a rich and ever-expanding universe where a
+            vibrant array of products converge into an interconnected overlay
+            experience on your world.
+          </p>{" "}
         </div>
-        <p className="max-w-md font-curcularWeb text-lg text-blue-50 opacity-50">
-          Immerse yourself in a rich and ever-expanding universe where a vibrant
-          array of products converge into an interconnected overlay experience
-          on your world.
-        </p>
+        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <Bentocard
+            src="../videos/feature-1.mp4"
+            title={
+              <>
+                radi<b>n</b>t
+              </>
+            }
+            describtion="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+            isComingSoon={true}
+          />
+        </div>
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <Bentocard
+              src="../videos/feature-2.mp4"
+              title={
+                <>
+                  zi<b>g</b>ma
+                </>
+              }
+              describtion="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
